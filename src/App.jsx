26fd3d6930cf16c708e5cbfc739a99dc2305347f
AbdoCodesC <abdeerbusiness@gmail.com>
2026-06-23 12:18:21 -0400

    Add Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/auth.jsx";
+import Checkout from "./pages/checkout.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
